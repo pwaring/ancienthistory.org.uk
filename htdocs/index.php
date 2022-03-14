c7c7d35c190ea1c908098d0ef1ee2ed1ee7a6b6b
Paul Waring <paul@xk7.net>
2022-03-14 14:47:11 +0000

    Initial commit

---
+++ htdocs/index.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types = 1);
+
+use App\Controllers\PageController;
+use DI\Container;
+use Psr\Container\ContainerInterface;
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+use Twig\Extra\Intl\IntlExtension;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+if ($_ENV['APP_WHOOPS'] === 'on') {
+    $whoops = new \Whoops\Run;
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+    $whoops->register();
+}
+
+date_default_timezone_set($_ENV['APP_DEFAULT_TIMEZONE']);
+
+$container = new Container();
+AppFactory::setContainer($container);
+
+// Register all dependencies
+$container->set('view', function() {
+    $twig = Twig::create(
+        __DIR__ . '/../templates',
+        [
+            'strict_variables' => true
+        ]
+    );
+    $twig->addExtension(new IntlExtension());
+
+    return $twig;
+});
+
+// $container->set('database', function() {
+//     $connection = [
+//         'driver' => 'pdo_mysql',
+//         'user' => $_ENV['APP_DATABASE_USER'],
+//         'password' => $_ENV['APP_DATABASE_PASSWORD'],
+//         'dbname' => $_ENV['APP_DATABASE_NAME'],
+//         'host' => $_ENV['APP_DATABASE_HOST']
+//     ];
+
+//     $config = Doctrine\ORM\Tools\Setup::createAnnotationMetadataConfiguration([]);
+
+//     return Doctrine\ORM\EntityManager::create($connection, $config);
+// });
+
+// Register all controllers
+$container->set('PageController', function (ContainerInterface $container) {
+    $view = $container->get('view');
+
+    return new PageController($view);
+});
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+$app->addBodyParsingMiddleware();
+$app->add(TwigMiddleware::createFromContainer($app));
+
+$app->get('/', 'PageController:index')->setName('index');
+$app->get('/contact/', 'PageController:contact')->setName('contact');
+$app->get('/groups/', 'PageController:groups')->setName('groups.index');
+$app->get('/talks/', 'PageController:talks')->setName('talks.index');
+
+$app->run();
---
+++ src/Controllers/PageController.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Controllers;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Views\Twig;
+
+class PageController
+{
+    private $view;
+
+    public function __construct(Twig $view)
+    {
+        $this->view = $view;
+    }
+
+    public function index(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        return $this->view->render(
+            $response,
+            'index.twig.html',
+            []
+        );
+    }
+
+    public function contact(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        return $this->view->render(
+            $response,
+            'contact/index.twig.html',
+            []
+        );
+    }
+
+    public function groups(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        return $this->view->render(
+            $response,
+            'groups/index.twig.html',
+            []
+        );
+    }
+
+    public function talks(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        return $this->view->render(
+            $response,
+            'talks/index.twig.html',
+            []
+        );
+    }
+}
