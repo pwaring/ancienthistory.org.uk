50c5cb9a1d184018b15a6459cf1d728428fd8836
Paul Waring <paul@xk7.net>
2022-08-29 13:36:43 +0100

    Add error middleware in production
    This overrides Whoops for 404 errors

--- htdocs/index.php
+++ htdocs/index.php
@@ -7,6 +7,7 @@ use DI\Container;
 use Doctrine\ORM\ORMSetup;
 use Psr\Container\ContainerInterface;
 use Slim\Factory\AppFactory;
+use Slim\Middleware\ErrorMiddleware;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 use Twig\Extra\Intl\IntlExtension;
@@ -16,7 +17,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-if ($_ENV['APP_WHOOPS'] === 'on') {
+define('WHOOPS_ENABLED', isset($_ENV['APP_WHOOPS']) && $_ENV['APP_WHOOPS'] === 'on');
+
+if (WHOOPS_ENABLED) {
     $whoops = new \Whoops\Run();
     $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
     $whoops->register();
@@ -66,6 +69,18 @@ $app = AppFactory::create();
 $app->addRoutingMiddleware();
 $app->addBodyParsingMiddleware();
 $app->add(TwigMiddleware::createFromContainer($app));
+
+if (!WHOOPS_ENABLED) {
+    $errorMiddleware = new ErrorMiddleware(
+        $app->getCallableResolver(),
+        $app->getResponseFactory(),
+        false,
+        true,
+        true
+    );
+
+    $app->add($errorMiddleware);
+}
 
 $app->get('/', PageController::class . ':index')->setName('index');
 $app->get('/about/', PageController::class . ':about')->setName('about');
