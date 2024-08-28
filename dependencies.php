772eed917fd3fc3fc942666ae0ee24032445dbd5
Paul Waring <paul@xk7.net>
2024-08-28 16:47:57 +0100

    Check required environment variables after loading

--- dependencies.php
+++ dependencies.php
@@ -10,10 +10,10 @@ use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 
 $dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 $dotenv->required([
     'APP_DEFAULT_TIMEZONE'
 ]);
-$dotenv->load();
 
 if (isset($_ENV['APP_WHOOPS']) && $_ENV['APP_WHOOPS'] === 'on') {
     $whoops = new \Whoops\Run();
