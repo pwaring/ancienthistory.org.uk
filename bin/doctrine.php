bb427a002022508a000e7c79a4f1fb80e322fbb1
Paul Waring <paul@xk7.net>
2024-08-30 15:11:56 +0100

    Doctrine script to run migrations etc.

---
+++ bin/doctrine.php
@@ -0,0 +1,12 @@
+<?php
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
+use Doctrine\ORM\Tools\Console\EntityManagerProvider\SingleManagerProvider;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../dependencies.php';
+
+ConsoleRunner::run(
+    new SingleManagerProvider($container->get(EntityManager::class))
+);
